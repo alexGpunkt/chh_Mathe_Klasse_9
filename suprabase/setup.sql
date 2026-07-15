1c5c07c21a89f4dd89fbe6ae7ce9fb3a8df91acf
alexGpunkt <alexander.goehl@googlemail.com>
2026-07-15 20:56:13 +0200

    Update setup.sql

--- suprabase/setup.sql
+++ suprabase/setup.sql
@@ -2,7 +2,7 @@
 window.MATHE9_SUPABASE = {
   url: 'https://vizobxvwvpeplvftgnua.supabase.co',
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpem9ieHZ3dnBlcGx2ZnRnbnVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxMzMxMDUsImV4cCI6MjA5OTcwOTEwNX0.nhIv_2CTdRAb02CZyy6BRGOIoj5azoQIe9Njmymg3Pg',
-  enabled: false,
+  enabled: true,
   heartbeatSeconds: 20,
-  classCode: '9'
+  classCode: 'M456_9er'
 };
