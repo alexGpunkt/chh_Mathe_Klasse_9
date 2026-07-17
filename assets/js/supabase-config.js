f51f551cda38cca3e78210c078cf3a73d5c818e8
alexGpunkt <alexander.goehl@googlemail.com>
2026-07-17 19:23:50 +0200

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -3,7 +3,7 @@ window.MATHE9_SUPABASE = {
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpem9ieHZ3dnBlcGx2ZnRnbnVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxMzMxMDUsImV4cCI6MjA5OTcwOTEwNX0.nhIv_2CTdRAb02CZyy6BRGOIoj5azoQIe9Njmymg3Pg',
   enabled: true,
   heartbeatSeconds: 20,
-  classCode: '9'
+  classCode: '9',
   devMode: true,
   devClassCode: 'DEV'
 };
