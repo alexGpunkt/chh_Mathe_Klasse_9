5f0e6fa5c4359aee183b4969b61b85724a5d12fe
alexGpunkt <alexander.goehl@googlemail.com>
2026-07-17 20:29:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    Sonst sehen die Geräte weiter die alte Fassung.
    ============================================================ */
 
-const VERSION = 'mathe9-v6-devtools';
+const VERSION = 'mathe9-v7-devtools';
 
 const SCHALE = [
   './',
