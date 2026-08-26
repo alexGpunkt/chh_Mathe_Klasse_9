5b1e32b78d63931736f2035edf5176f27715613e
alexGpunkt <alexander.goehl@googlemail.com>
2026-08-26 19:59:31 +0200

    Supabase-Produktion konfigurieren

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,9 +1,10 @@
 window.MATHE9_SUPABASE = {
   url: 'https://vizobxvwvpeplvftgnua.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpem9ieHZ3dnBlcGx2ZnRnbnVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxMzMxMDUsImV4cCI6MjA5OTcwOTEwNX0.nhIv_2CTdRAb02CZyy6BRGOIoj5azoQIe9Njmymg3Pg',
+  anonKey: 'sb_publishable_tdVrNZQ9vIEo-Oz2GXiY0w_W_hopuUw',
   enabled: true,
   heartbeatSeconds: 20,
   classCode: '9',
-  devMode: true,
+  devMode: false,
   devClassCode: 'DEV'
 };
+
